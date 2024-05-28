09a5da3e26ba0e193a01825bff3e0481465c44ac
Fabio Angelici <fabio.angelici@outlook.it>
2024-05-28 23:38:25 +0200

    Creato ccontroller carello, rotte e js per aggiunta e rimozione prodotti dal carrello

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(ProductController::class)->group(function () {
-    Route::get('/', 'index')->name('home');
-    Route::get('/product/{product:slug}', 'view')->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function () {
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('/', 'index')->name('home');
+        Route::get('/product/{product:slug}', 'view')->name('product.view');
+    });
+
+    Route::prefix('/cart')->name('cart.')->group(function () {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::post('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::post('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::post('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
 });
+
 
 
 
