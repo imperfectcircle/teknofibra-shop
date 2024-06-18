b7dd04bd07d8408b29965077a464f0fe1d8da168
Fabio Angelici <fabio.angelici@outlook.it>
2024-06-18 23:04:46 +0200

    Modifiche alla hp

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,40 +5,20 @@ $categoryList = \App\Models\Category::getActiveAsTree();
 ?>
 
 <x-app-layout>
+
+    {{-- <div class="hero w-full grid place-items-center -m-5">
+        
+
+    </div> --}}
     
     <div id="controls-carousel" class="relative w-full" data-carousel="static">
         <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden md:min-h-[65vh] bg-center bg-no-repeat bg-cover bg-custom-gradient-image -mt-5 -mb-5 -ml-5 -mr-5">
+        <div class="relative h-[50vh] overflow-hidden md:min-h-[65vh] bg-center bg-no-repeat bg-cover bg-custom-gradient-image -mt-5 -mb-5 -ml-5 -mr-5 grid place-items-center">
             <!-- Item 1 -->
-            @foreach ($products as $item)
-            <div class="hidden duration-700 ease-in-out m-10" data-carousel-item>
-                <a href="{{ route('product.view', $item->slug) }}">
-                    @if ($item->images && count($item->images) > 0)
-                        <img src="{{ $item->image  }}" class="absolute block w-full h-full object-contain -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $item->title }}">
-                    @else
-                        <img src="/img/noimage.png" class="absolute block w-full h-full object-contain -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="{{ $item->title }}">
-                    @endif
-                </a>
-            </div>
-            @endforeach
+            <h1 class="my-10 text-center text-xl md:text-3xl lg:text-4xl font-semibold text-white">Scopri le Soluzioni Isolanti di Teknofibra: <br> Acquista Ora i Migliori Prodotti per l'Automotive e l'Aviazione!</h1>
         </div>
-        <!-- Slider controls -->
-        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
+        
+        
     </div>
     
     <x-category-list :category-list="$categoryList" class="-ml-5 mt-5 -mr-5 px-4"/>
@@ -93,11 +73,9 @@ $categoryList = \App\Models\Category::getActiveAsTree();
     </div>
     <?php else: ?>
 
-    <h1 class="my-10 text-center text-3xl font-semibold">Scopri le Soluzioni Isolanti di Teknofibra: <br> Acquista Ora i Migliori Prodotti per l'Automotive e l'Aviazione!</h1>
-
     <!-- Product List -->
     <div
-    class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5"
+    class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-5 pt-10 place-items-center"
     >
     
         @foreach ($products as $product )
--- resources/views/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-<x-app-layout>
-    welcome
-</x-app-layout>
