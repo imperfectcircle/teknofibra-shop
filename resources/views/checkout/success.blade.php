901d6995cd78b85a3fceccd1a7bcc0d9cd42c05f
Fabio Angelici <fabio.angelici@outlook.it>
2024-07-12 18:25:28 +0200

    sistemato stile thank you page

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -5,8 +5,8 @@
         <p class="text-2xl text-emerald-600">Il tuo ordine è andato a buon fine!</p>
         <p class="text-xl text-emerald-600">A breve riceverai un'email con il riepilogo del tuo ordine.</p>
         <div class="flex py-5 space-x-3">
-            <a class="px-6 py-2 rounded shadow border border-emerald-600 hover:bg-emerald-500 hover:text-white transition-colors" href="{{ route('home') }}">Torna alla Home</a>
-            <a class="px-6 py-2 rounded shadow border border-emerald-600 hover:bg-emerald-500 hover:text-white transition-colors" href="{{ route('orders.index') }}">Vai ai tuoi Ordini</a>
+            <a class="px-6 py-2 rounded shadow border text-black border-emerald-600 hover:bg-emerald-500 hover:text-white transition-colors" href="{{ route('home') }}">Torna alla Home</a>
+            <a class="px-6 py-2 rounded shadow border text-black border-emerald-600 hover:bg-emerald-500 hover:text-white transition-colors" href="{{ route('orders.index') }}">Vai ai tuoi Ordini</a>
         </div>
     </div>
 </x-app-layout>
