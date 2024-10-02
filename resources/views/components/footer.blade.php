79ed3ffd87534cbfb277fcdc596e1bf1f28173a0
Fabio Angelici <fabio.angelici@outlook.it>
2024-10-02 22:09:56 +0200

    corretto bug

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -89,6 +89,7 @@ $categoryList = \App\Models\Category::getActiveAsTree();
                         <a href="https://www.iubenda.com/termini-e-condizioni/82606974" class="iubenda-nostyle no-brand iubenda-noiframe iubenda-embed iubenda-noiframe " title="Termini e Condizioni ">{{ __('footer.terms') }}</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
                     @else
                         <a href="https://www.iubenda.com/terms-and-conditions/72210443" class="iubenda-nostyle no-brand iubenda-noiframe iubenda-embed iubenda-noiframe " title="Terms and Conditions ">{{ __('footer.terms') }}</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+                    @endif
                 </div>
             </div>
         </div>
